292b9634b9f4fc619efb0ac0dae847863167cb19
Zelak <zelak312@gmail.com>
2022-02-09 13:11:42 +0000

    Fixing ForStatement

--- src/ast/nodes/for_statement.rs
+++ src/ast/nodes/for_statement.rs
@@ -10,13 +10,15 @@ pub struct NForStatement {
 impl NForStatement {
     pub fn debug(&self, tab: usize) {
         println!("{}{}", "\t".repeat(tab), "ForStatement");
-        self.start
-            .as_ref()
-            .map_or_else(|| println!("None"), |n| n.debug(Some(tab + 1)));
+        self.start.as_ref().map_or_else(
+            || println!("{}None", "\t".repeat(tab + 1)),
+            |n| n.debug(Some(tab + 1)),
+        );
         self.condition.debug(Some(tab + 1));
-        self.step
-            .as_ref()
-            .map_or_else(|| println!("None"), |n| n.debug(Some(tab + 1)));
+        self.step.as_ref().map_or_else(
+            || println!("{}None", "\t".repeat(tab + 1)),
+            |n| n.debug(Some(tab + 1)),
+        );
         for expression in &self.expressions {
             expression.debug(Some(tab + 2));
         }
